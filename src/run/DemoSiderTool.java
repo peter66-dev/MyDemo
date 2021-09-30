6b250dc0d2e37c05e3b77664c8929d68fdb28479
phuxiteo3 <vanphuong0606@gmail.com>
2021-09-30 09:20:13 +0700

    MyDemo

---
+++ src/run/DemoSiderTool.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package run;
+
+import java.util.Scanner;
+import sample.library.MyMath;
+
+/**
+ *
+ * @author Dell
+ */
+public class DemoSiderTool {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter a: ");
+        int a = Integer.parseInt(scan.nextLine());
+        System.out.print("Enter b: ");
+        int b = Integer.parseInt(scan.nextLine());
+        a = 10;
+        b = 2;
+        int resultSum = MyMath.Sum(a, b);
+        int resultSub = MyMath.Sub(a, b);
+        System.out.printf("Sub: %d - %d = %d", a, b, resultSub);
+        System.out.println();
+    }
+
+}
---
+++ src/sample/library/MyMath.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sample.library;
+
+/**
+ *
+ * @author Dell
+ */
+public class MyMath {
+
+    public static int Sum(int a, int b) {
+        return a + b;
+    }
+
+    public static int Sub(int a, int b) {
+        return a - b;
+    }
+
+}
