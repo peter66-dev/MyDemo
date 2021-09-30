ddabf5714dffd3093828359e93ab5edaa4c9ed67
phuxiteo3 <vanphuong0606@gmail.com>
2021-09-30 09:55:43 +0700

    secenddd

--- src/sample/library/DemoSiderTool.java
+++ src/sample/library/DemoSiderTool.java
@@ -18,17 +18,7 @@ public class DemoSiderTool {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        System.out.print("Enter a: ");
-        int a = Integer.parseInt(scan.nextLine());
-        System.out.print("Enter b: ");
-        int b = Integer.parseInt(scan.nextLine());
-        a = 10;
-        b = 2;
-        int resultSum = MyMath.Sum(a, b);
-        int resultSub = MyMath.Sub(a, b);
-        System.out.printf("Sub: %d - %d = %d", a, b, resultSub);
-        System.out.println();
+        
     }
 
 }
