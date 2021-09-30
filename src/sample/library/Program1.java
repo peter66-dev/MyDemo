25170042b235f7faa3138817b9646cb52777f91a
phuxiteo3 <74146862+phuxiteo3@users.noreply.github.com>
2021-09-30 10:08:41 +0700

    Add files via upload

---
+++ src/sample/library/Program1.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sample.library;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Dell
+ */
+public class Program1 {
+
+    public static void main(String[] args) {
+        int a, b;
+        a = 10;
+        b = 2;
+        if (a < 0) {
+            System.out.println("Ahihi");
+        }
+        int resultSum = MyMath.Sum(a, b);
+        int resultSub = MyMath.Sub(a, b);
+        System.out.printf("Sub: %d - %d = %d", a, b, resultSub);
+        System.out.println();
+    }
+}
